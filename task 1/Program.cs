99b1a18f0e7e0e6d85cbb7ff2f258fdcc63a7097
Pavlyuk <a@b.c>
2023-07-15 00:12:56 +0400

    dz6

---
+++ task 1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Сколько чисел необходимо проверить? - ");
+int mas = int.Parse(Console.ReadLine());
+Console.Write("Вводите по одному: ");
+int plus = 0;
+int[] Arr = new int[mas];
+for (int i=0; i < Arr.Length; i++) 
+{   
+    Arr[i] = int.Parse(Console.ReadLine());
+    if (Arr[i] > 0) plus++;
+    if(i < Arr.Length-1) Console.Write("Следующее: ");
+}
+
+Console.Write("[");
+for (int i=0; i < Arr.Length; i++)
+{
+    Console.Write(Arr[i]);
+    if(i < Arr.Length-1) Console.Write(", ");
+}
+Console.Write("]");
+if(plus != 1){
+    Console.Write($" -> из них {plus} положительных.");
+}
+else{
+    Console.Write($" -> из них {plus} положительное.");
+}
---
+++ task 2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите значения первой прямой.");
+Console.Write("b1 = ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("k1 = ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значения первой прямой.");
+Console.Write("b2 = ");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("k2 = ");
+double k2 = double.Parse(Console.ReadLine());
+if (k1 == k2) {Console.WriteLine("Линии параллельны");}
+else {
+double x = ((b2 - b1) / (k1 - k2));
+double y1 = (k1 * x + b1);
+double y2 = (k2 * x + b2);
+if (y1 == y2) {
+Console.WriteLine($"Координаты точки пересечения ({x}; {y1})"); }
+else {Console.WriteLine("Не удалось найти точки пересечения");}
+}
